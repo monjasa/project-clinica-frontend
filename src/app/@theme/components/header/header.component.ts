8297544700876db8b4a812f475a19c703f860fd0
monjasa <monja.nya@gmail.com>
2021-04-26 23:12:10 +0300

    Initial commit

---
+++ src/app/@theme/components/header/header.component.ts
@@ -0,0 +1,56 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { NbMediaBreakpointsService, NbMenuService, NbSidebarService, NbThemeService } from '@nebular/theme';
+
+import { UserData } from '../../../@core/data/users';
+import { map, takeUntil } from 'rxjs/operators';
+import { Subject } from 'rxjs';
+
+@Component({
+  selector: 'ngx-header',
+  styleUrls: ['./header.component.scss'],
+  templateUrl: './header.component.html',
+})
+export class HeaderComponent implements OnInit, OnDestroy {
+
+  userPictureOnly: boolean = false;
+  user: any;
+  userMenu = [{ title: 'Profile' }, { title: 'Log out' }];
+  private destroy$: Subject<void> = new Subject<void>();
+
+  constructor(private sidebarService: NbSidebarService,
+              private menuService: NbMenuService,
+              private themeService: NbThemeService,
+              private userService: UserData,
+              private breakpointService: NbMediaBreakpointsService) {
+  }
+
+  ngOnInit() {
+    this.userService.getUsers()
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((users: any) => this.user = users.eva);
+
+    const { xl } = this.breakpointService.getBreakpointsMap();
+    this.themeService.onMediaQueryChange()
+      .pipe(
+        map(([, currentBreakpoint]) => currentBreakpoint.width < xl),
+        takeUntil(this.destroy$),
+      )
+      .subscribe((isLessThanXl: boolean) => this.userPictureOnly = isLessThanXl);
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+
+  toggleSidebar(): boolean {
+    this.sidebarService.toggle(true, 'menu-sidebar');
+
+    return false;
+  }
+
+  navigateHome() {
+    this.menuService.navigateHome();
+    return false;
+  }
+}
---
+++ src/app/@theme/theme.module.ts
@@ -0,0 +1,70 @@
+import { ModuleWithProviders, NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {
+  NbActionsModule,
+  NbButtonModule,
+  NbContextMenuModule,
+  NbIconModule,
+  NbLayoutModule,
+  NbMenuModule,
+  NbSearchModule,
+  NbSelectModule,
+  NbSidebarModule,
+  NbThemeModule,
+  NbUserModule,
+} from '@nebular/theme';
+import { NbEvaIconsModule } from '@nebular/eva-icons';
+import { NbSecurityModule } from '@nebular/security';
+
+import { FooterComponent, HeaderComponent } from './components';
+import { CapitalizePipe, NumberWithCommasPipe, PluralPipe, RoundPipe, TimingPipe } from './pipes';
+import { OneColumnLayoutComponent } from './layouts';
+import { DEFAULT_THEME } from './styles/theme.default';
+
+const NB_MODULES = [
+  NbLayoutModule,
+  NbMenuModule,
+  NbUserModule,
+  NbActionsModule,
+  NbSearchModule,
+  NbSidebarModule,
+  NbContextMenuModule,
+  NbSecurityModule,
+  NbButtonModule,
+  NbSelectModule,
+  NbIconModule,
+  NbEvaIconsModule,
+];
+const COMPONENTS = [
+  HeaderComponent,
+  FooterComponent,
+  OneColumnLayoutComponent,
+];
+const PIPES = [
+  CapitalizePipe,
+  PluralPipe,
+  RoundPipe,
+  TimingPipe,
+  NumberWithCommasPipe,
+];
+
+@NgModule({
+  imports: [CommonModule, ...NB_MODULES],
+  exports: [CommonModule, ...PIPES, ...COMPONENTS],
+  declarations: [...COMPONENTS, ...PIPES],
+})
+export class ThemeModule {
+  static forRoot(): ModuleWithProviders<ThemeModule> {
+    return {
+      ngModule: ThemeModule,
+      providers: [
+        ...NbThemeModule.forRoot(
+          {
+            name: 'default',
+          },
+          [DEFAULT_THEME],
+        ).providers,
+      ],
+    };
+  }
+}
